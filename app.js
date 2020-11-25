22513babbc6e62dd6c574efc87132bb17af2a6bd
Youngmi <youngmi9347@gmail.com>
2020-11-25 13:35:05 +0900

    wetube(~3.4)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import { localsMiddlewares } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -11,13 +11,13 @@ import globalRouter from "./routers/globadlRouter";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug")
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddlewares);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
