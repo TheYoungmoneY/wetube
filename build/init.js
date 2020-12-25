033c28674f484a44588acad07e28f5c3422e19ba
Youngmi <youngmi9347@gmail.com>
2020-12-25 22:47:03 +0900

    HEROKU

---
+++ build/init.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+require("./db");
+
+var _app = _interopRequireDefault(require("./app"));
+
+require("./models/Video");
+
+require("./models/User");
+
+require("./models/Comment");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_dotenv["default"].config();
+
+var PORT = process.env.PORT || 4000;
+
+var handleListening = function handleListening() {
+  console.log("Listening on: http://localhost:".concat(PORT));
+};
+
+_app["default"].listen(PORT, handleListening);
