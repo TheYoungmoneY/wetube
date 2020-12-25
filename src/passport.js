5a01e1860daa16ab67978d9cd7f3669ac6b8f6cf
Youngmi <youngmi9347@gmail.com>
2020-12-25 23:18:27 +0900

    localhost 주소 heroku로  변경

--- src/passport.js
+++ src/passport.js
@@ -10,7 +10,9 @@ passport.use(User.createStrategy());
 passport.use(new GithubStrategy({
     clientID: process.env.GH_ID,
     clientSecret: process.env.GH_SECRET,
-    callbackURL: `http://localhost:4000${routes.githubCallback}`
+    callbackURL: process.env.PRODUCTION
+        ? `https://secure-anchorage-45478.herokuapp.com/${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`
 }, githubLoginCallback
 ));
 passport.use(new FacebookStrategy({
